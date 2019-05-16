717de84a291bc2d70990a591faf076a588726717
ByeongwooAhn <50216870+ByeongwooAhn@users.noreply.github.com>
2019-05-17 00:06:00 +0900

    안병우
    20190517

---
+++ dojang report/sub/33강 심사문제.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNING
+
+int main()
+{
+	int num1, num2;
+
+	scanf("%d %d", &num1, &num2);
+
+	for (int i = num1; i <= num2; i++)
+	{
+		if (i % 5 == 0 && i % 11 == 0)
+		{
+			printf("FizzBuzz\n");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Fizz\n");
+		}
+		else if (i % 11 == 0)
+		{
+			printf("Buzz\n");
+		}
+		else
+		{
+			printf("%d\n", i);
+		}
+	}
+}
