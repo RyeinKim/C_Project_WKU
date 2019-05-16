a74bd3b4c21a0d6bb75fd9589d0d9b6cbfa33ec3
ByeongwooAhn <50216870+ByeongwooAhn@users.noreply.github.com>
2019-05-16 22:45:35 +0900

    안병우
    20190516

---
+++ dojang report/sub/28강 심사문제.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNING
+
+int main()
+{
+	int money;
+
+	scanf("%d", &money);
+
+	while (money >= 1200)
+	{
+		money -= 1200;
+		printf("%d\n", money);
+	}
+
+	return 0;
+}
