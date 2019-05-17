995979bf42ee00ce60da293c27354aa1aba0112f
MinJai-Kim <thsutleo0916@gmail.com>
2019-05-17 19:35:54 +0900

    김민지
    20190517 김민지

---
+++ dojang report/dev3/17.8 심사문제.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int a;
+
+	scanf("%d", &a);
+
+	if (a < 18) {
+		printf("청소년 관람 불가");
+	}
+}
---
+++ dojang report/dev3/18.9 심사문제.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	char c;
+	scanf("%c", &c);
+	if (c == 'a') {
+		printf("a입니다");
+	}
+	else {
+		printf("a가 아닙니다");
+	}
+}
