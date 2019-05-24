6ab8609cb0c49bdf3063e8e20d4f110614a1500c
SeogyeongLee <seogyeong0612@gmail.com>
2019-05-24 14:01:07 +0900

    20190524 - 이서경
    이서경

---
+++ dojang report/dev2/Unit_21.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int num1, num2;
+	scanf("%d%d", &num1, &num2);
+	if (num1&&num2)
+	{
+		printf("Âü\n");
+	}
+	else
+	{
+		printf("°ÅÁş\n");
+	}
+	if (num1 || num2)
+	{
+		printf("Âü\n");
+	}
+	else
+	{
+		printf("°ÅÁş\n");
+	}
+	if (!num1)
+	{
+		printf("Âü\n");
+	}
+	else
+	{
+		printf("°ÅÁş\n");
+	}
+	return 0;
+}
