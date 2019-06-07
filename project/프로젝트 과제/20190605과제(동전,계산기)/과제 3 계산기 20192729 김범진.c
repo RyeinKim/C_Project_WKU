10071fd0aeb346f2a33f419e4eba4b7f48429bf5
Bumjin-Kim <50228892+Bumjin-Kim@users.noreply.github.com>
2019-06-07 20:50:33 +0900

    Create 과제 3 계산기 20192729 김범진.c

---
+++ project/프로젝트 과제/20190605과제(동전,계산기)/과제 3 계산기 20192729 김범진.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int add(int a, int b);
+int sub(int a, int b);
+int dis(int a, int b);
+int mul(int a, int b);
+struct bumzani {
+	int st, nd, num1, num2, num3, num4;
+	int (*ADD)(int, int);
+	int (*SUB)(int, int);
+	int (*DIS)(int, int);
+	int (*MUL)(int, int);
+	char Sym;
+};
+int main() {
+	while (1) {
+		struct bumzani A;
+		printf("숫자를 입력하세요 = ");
+		scanf("%d", &A.st);
+		printf("문자 입력하세요 = ");
+		scanf(" %c", &A.Sym);
+		printf("숫자를 입력하세요 = ");
+		scanf("%d", &A.nd);
+
+		A.ADD = add;
+		A.SUB = sub;
+		A.DIS = dis;
+		A.MUL = mul;
+
+		if (A.Sym == '+')
+			printf("%d\n\n", A.ADD(A.st, A.nd));
+		else if (A.Sym == '-')
+			printf("%d\n\n", A.SUB(A.st, A.nd));
+		else if (A.Sym == '/')
+			printf("%d\n\n", A.DIS(A.st, A.nd));
+		else if (A.Sym == '*')
+			printf("%d\n\n", A.MUL(A.st, A.nd));
+		else
+			printf("오류\n\n");
+	}
+}
+int add(int a, int b) {
+	return a + b;
+}
+int sub(int a, int b) {
+	return a - b;
+}
+int dis(int a, int b) {
+	return a / b;
+}
+int mul(int a, int b) {
+	return a * b;
+}
